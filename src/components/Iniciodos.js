57e3a010018ac21396e03eb1a505a89480f81637
Jose Sanchez <ripaiple_jam@hotmail.com>
2022-01-28 17:13:49 -0500

    actualizando componente Iniciodos

--- src/components/Iniciodos.js
+++ src/components/Iniciodos.js
@@ -2,25 +2,14 @@ import { DivPadre } from '../container/Container';
 import React from 'react';
 import styled from 'styled-components';
 
-
-const H1 = styled.h1`
- color: white;
- font-family: 'Inter';
- text-align: center;
-`
-
 const Label = styled.label`
   color: white;
 `
 
 export const Iniciodos = () => {
-
-    
-    let contador = 1
   
       return (
         <DivPadre>
-          {/*   <H1>{qHtml[contador].pregunta}</H1> */}
             <form  method="post" target="_blank">
               <Label><input type="radio" checked id="respuesta1" name="check" />Main</Label>
               <Label><input type="radio" id="respuesta2" name="check" />Section</Label>
